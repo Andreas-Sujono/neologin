e92ff602c25167f96417be7e5dcf05a59b0448d0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-05 19:52:11 +0000

    Deploy safudex/neologin to github.com/safudex/neologin.git:gh-pages

---
+++ widget/precache-manifest.e9b438c2d8e2f1c6b5f2d19f24c4b149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932b49fb1c48d1c941086cd9d761d0e5",
+    "url": "/widget/index.html"
+  },
+  {
+    "revision": "2993b0a5f21ab30b1e05",
+    "url": "/widget/static/css/main.83fda8c5.chunk.css"
+  },
+  {
+    "revision": "b4eb352907ad3a947cbd",
+    "url": "/widget/static/js/2.44129a42.chunk.js"
+  },
+  {
+    "revision": "2993b0a5f21ab30b1e05",
+    "url": "/widget/static/js/main.fe23555b.chunk.js"
+  },
+  {
+    "revision": "f3230c88af25cd1c504e",
+    "url": "/widget/static/js/runtime~main.6336db6d.js"
+  }
+]);
